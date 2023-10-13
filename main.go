f7d4b7559db3955b4d4c2b9f4a8dfb75636786d4
Morteza Jamshidi <devmor.j@gmail.com>
2023-10-13 21:57:41 +0330

    create a basic api group

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
+	flag.Parse()
+
+	app := fiber.New()
+	apiV1 := app.Group("/api/v1")
+
+	apiV1.Get("/user", handleUser)
+
+	app.Listen(*listenAddr)
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "James Bond"})
 }
