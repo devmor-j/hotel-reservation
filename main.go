b3a64f31adfc5188e07df95bdc6a6f2951eaff28
Morteza Jamshidi <devmor.j@gmail.com>
2023-10-19 20:41:55 +0330

    write a basic error handler

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ import (
 
 const dburi = "mongodb://localhost:27017"
 
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
@@ -29,7 +35,7 @@ func main() {
 		}
 	}()
 
-	app := fiber.New()
+	app := fiber.New(config)
 	apiV1 := app.Group("/api/v1")
 
 	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
